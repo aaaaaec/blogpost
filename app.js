e13a521c835c433546d18a3f029ce868f15adac8
aaaaaec <zephyeh@gmail.com>
2022-10-23 15:23:33 +0800

    section30

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+//add mongoose
+const mongoose = require("mongoose");
+//lodash
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const aboutContent = "Daily Journal created for you to write down your thoughts and feelings. ";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
@@ -15,66 +19,58 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//connect a mongodb and to todolistDB database
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true
+});
 
-
-//lodash
-// Load the full build.
-const _ = require('lodash');
-
-
-
-
-
-let posts = [];
+const postSchema = {
+ title: String,
+ content: String
+};
+const Post = mongoose.model("Post", postSchema);
 
 
 app.get("/",function(req,res){
-  res.render("home",{startingContent: homeStartingContent, posts:posts} );
-});
-
-
-
-app.get("/about", function(req,res){
-  res.render("about",{aboutContent: aboutContent} );
-});
-
-
-
-app.get("/contact", function(req,res){
-  res.render("contact",{contactContent: contactContent} );
+  Post.find({}, function(err, posts){
+   res.render("home", {
+     startingContent: homeStartingContent,
+     posts: posts
+     });
+ })
 });
 
 //client requesting for /compose
 app.get("/compose", function(req,res){
   res.render("compose"); //render the page of compose.ejs
 });
+
+
 //server sending back /compose
 app.post("/compose", function(req,res){
-  //create a javascript object to pass in title and post
-  const post = {title:req.body.postTitle, content:req.body.postBody};
-  //add into array
-  posts.push(post);
-  //redirect to home page
-  res.redirect("/");
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  //save the user's composed post and if there's no error, redirect to home page
+  post.save(function(err){
+    if(!err){
+      //redirect to home page
+      res.redirect("/");
+    }
+  });
 });
 
 
 //dynamic website for exmaple if day-1 is logged, will be printed in console. http://localhost:3000/posts/day-1
-app.get("/posts/:postName",function(req,res){
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId",function(req,res){
+  const requestedPostId = req.params.postId;
 
-  //for each element in array posts, check the title is equal to the requested title
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle){
-
-      res.render("post",{
-        title: post.title,
-        content: post.content
-      });
-
-    }
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
   });
 
 });
@@ -82,7 +78,13 @@ app.get("/posts/:postName",function(req,res){
 
 
 
+app.get("/about", function(req,res){
+  res.render("about",{aboutContent: aboutContent} );
+});
 
+app.get("/contact", function(req,res){
+  res.render("contact",{contactContent: contactContent} );
+});
 
 
 
